35b87cd029648161398a9a68470de287243c86d3
Abdulwakeel Kesington <abdulwakeelkesington@gmail.com>
2026-04-17 10:08:26 +0100

    selection sort code and a little modification in the naming of bubblesort function

--- bubbleSort.py
+++ bubbleSort.py
@@ -3,7 +3,7 @@
 
 a = [-5, 3, 2, 1, -3, -3, 7, 2, 2]
 
-def bubble_Sort(arr):
+def bubble_sort(arr):
     n = len(arr)
     flag = True
     while flag:
@@ -11,7 +11,7 @@ def bubble_Sort(arr):
         for i in range(1, n):
             if arr[i-1] > arr[i]:
                 flag = True
-                arr[i -1], arr[i] = arr[i], arr[i-1]
+                arr[i-1], arr[i] = arr[i], arr[i-1]
     return arr, n
 print(bubble_Sort(a))
 
